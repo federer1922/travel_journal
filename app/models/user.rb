405c1f8c5bae686d456cba8dd6cadda78374f2af
Arkadiusz Karpinski <arekkarpinski2@gmail.com>
2021-12-21 22:20:54 +0100

    removing binding

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   has_many :notes, dependent: :destroy
 
   def self.from_google(email:, full_name:, uid:, avatar_url:)
-    binding.pry
     create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email) do |user|
       user.password = SecureRandom.urlsafe_base64
     end 
